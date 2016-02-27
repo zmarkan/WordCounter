ba23b5f5ed235580e28669d8fc6060e86c128648
zan <zan.markan@gmail.com>
2016-02-27 04:45:15 +0000

    Add test class and read lines from book

--- src/main/kotlin/WordCounter.kt
+++ src/main/kotlin/WordCounter.kt
@@ -1,5 +1,24 @@
+import java.io.File
+import java.util.*
 
 //
 fun main(args: Array<String>){
-    println("Hello World!")
+    //TODO: read file
+    //TODO: Map
+    //TODO: Reduce
+    //TODO: bask in glory
+
+    //Reading files
+    //Assume words are not split between the lines so we can just read lines
+    //TODO: have some easily configurable repository of files etc
+    val file = File("assets/Railway-Children-by-E-Nesbit.txt")
+
+    println(file.absolutePath)
+
+
+    //now we have a bunch of strings
+    for (line in file.readLines()){
+        println(line)
+    }
+
 }
---
+++ src/test/kotlin/WordCounterTest.kt
@@ -0,0 +1,13 @@
+import org.junit.Test
+import org.junit.Assert
+
+
+public class WordCounterTest {
+    @Test fun testThatFails() {
+        Assert.assertTrue(false)
+    }
+
+    @Test fun testThatPasses(){
+        Assert.assertTrue(true)
+    }
+}
